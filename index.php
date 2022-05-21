7943f7bad8152f206d533774eae8df859b8c7cd8
Yixon-Smith <yixon2011@gmail.com>
2022-05-21 02:02:19 -0400

    AgregarHome

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+
+<!DOCTYPE HTML>
+<html lang="zxx">
+<head>
+	<title>Sistema Unefa</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Full Screen Enroll Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+	<link rel="stylesheet" href="bootstrap.min.css" crossorigin="anonymous">
+	</script>
+	<link rel="stylesheet" href="_plantilla/css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="_plantilla/css/fontawesome-all.css">
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
+	<link rel="stylesheet" href="_plantilla/js/sweetalert.css">
+</head>
+<body class="fondoinicio">
+	
+</body>
+<script src="../vistas/_recursos/js/jquery.min.js"></script>
+<script src="../vistas/_recursos/js/consola_usuario.js"></script>
+<script src="_plantilla/js/sweetalert.min.js"></script>
+</html>
 <?php
-	 header('Location: Login/');
+	/* header('Location: Login/');*/
 ?>
